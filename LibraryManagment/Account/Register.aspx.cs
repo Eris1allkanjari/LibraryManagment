dbc48fc3c1b5a0c875ac7d0e151a04488e51a1d2
Eris1allkanjari <eris.allkanjari@gmail.com>
2020-01-02 14:06:10 +0100

    Changes at Register

--- LibraryManagment/Account/Register.aspx.cs
+++ LibraryManagment/Account/Register.aspx.cs
@@ -20,7 +20,7 @@ namespace LibraryManagment.Account
             DegaService degaService = new DegaService();
             var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
             var signInManager = Context.GetOwinContext().Get<ApplicationSignInManager>();
-            var user = new ApplicationUser() { UserName = Username.Text, Email = Email.Text };
+            var user = new ApplicationUser() { UserName = Email.Text, Email = Email.Text };
             IdentityResult result = manager.Create(user, Password.Text);
             if (result.Succeeded)
             {
